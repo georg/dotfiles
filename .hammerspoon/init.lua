2c44c07f34f9abc2ed12dece0c6e1cdbd1af7708
Georg Friedrich <georg.friedrich@redbubble.com>
2019-12-28 17:50:16 +1100

    Update Hammerspoon config
    Switch to official Spoons for clipboard history and window management.

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,38 +1,28 @@
-require "clipboard"
-
-local grid = require "hs.grid"
-
-local mash = {"cmd", "alt", "ctrl"}
-local mashShift = {"shift", "alt", "ctrl"}
+hs.loadSpoon("SpoonInstall")
+spoon.SpoonInstall:updateRepo('default')
 
 hs.window.animationDuration = 0
+local mash = {"cmd", "alt", "ctrl"}
+spoon.SpoonInstall:andUse('MiroWindowsManager', {
+  hotkeys = {
+    up = {mash, "up"},
+    right = {mash, "right"},
+    down = {mash, "down"},
+    left = {mash, "left"},
+    fullscreen = {mash, "c"},
+  },
+})
 
-hs.hotkey.bind(mash, "C", function()
-  hs.window.focusedWindow():moveToUnit({x=0.125, y=0.125, w=0.75, h=0.75})
-end)
+spoon.SpoonInstall:andUse('TextClipboardHistory', {
+  config = {
+    paste_on_select = true,
+  },
+  hotkeys = {
+    show_clipboard = {mash, "v"},
+  },
+  start = true,
+})
 
-hs.hotkey.bind(mash, 'M', grid.maximizeWindow)
-hs.hotkey.bind(mash, 'Left', function()
-  hs.window.focusedWindow():moveToUnit(hs.layout.left50)
-end)
-hs.hotkey.bind(mash, 'Right', function()
-  hs.window.focusedWindow():moveToUnit(hs.layout.right50)
-end)
-hs.hotkey.bind(mash, 'Up', function()
-  hs.window.focusedWindow():moveToUnit({x=0, y=0, w=1, h=0.5})
-end)
-hs.hotkey.bind(mash, 'Down', function()
-  hs.window.focusedWindow():moveToUnit({x=0, y=0.5, w=1, h=0.5})
-end)
-hs.hotkey.bind(mashShift, 'Left', function()
-  hs.window.focusedWindow():moveOneScreenEast()
-end)
-hs.hotkey.bind(mashShift, 'Right', function()
-  hs.window.focusedWindow():moveOneScreenWest()
-end)
-hs.hotkey.bind(mash, 'S', function()
-  local snapshot = hs.window.focusedWindow():snapshot()
-end)
 hs.hotkey.bind(mash, 'L', function()
   hs.caffeinate.lockScreen()
 end)
