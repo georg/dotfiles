7944081fa183d33dd3d4a793ab04f2a1f791e2ea
Georg Friedrich <georg.friedrich@redbubble.com>
2019-12-28 19:22:47 +1100

    Update dotfile setup

--- setup/mac.sh
+++ setup/mac.sh
@@ -3,4 +3,4 @@
 brew tap homebrew/cask-fonts
 brew cask install font-source-code-pro
 
-osascript set-terminal-theme.applescript
+osascript setup/set-terminal-theme.applescript
--- setup/setup.sh
+++ setup/setup.sh
@@ -3,7 +3,7 @@
 # http://stackoverflow.com/a/246128
 DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cd "$DOTFILES"
+cd "$DOTFILES/.."
 
 stow --verbose --stow . --target ~
 
